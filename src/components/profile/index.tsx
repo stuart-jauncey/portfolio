a98124a8533d8d3c362fac3f3e88bc9b00040b5c
Stu <stuartjauncey@hotmail.co.uk>
2024-09-08 16:50:35 +0100

    Refactor Profile component and add highlight

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,32 +1,50 @@
+'use client';
+
 import Image from 'next/image';
 
 import classes from './index.module.css';
+import { ReactElement } from 'react';
 
 export function Profile() {
+    const profile: ReactElement = (
+        <div className={classes.profile}>
+            <div className={classes.imageContainer}>
+                <Image
+                    src="https://res.cloudinary.com/stuj89/image/upload/v1725542567/Stu.jpg"
+                    alt="Stu"
+                    fill
+                    sizes="100%"
+                    priority
+                ></Image>
+            </div>
+            <div className={classes.titleContainer}>
+                <h1>Stuart Jauncey</h1>
+                <h2>Fullstack Software Developer</h2>
+            </div>
+        </div>
+    );
+
+    const description: ReactElement = (
+        <div className={classes.descriptionContainer}>
+            <p>Hi!</p>
+            <p>My name is Stuart and I am a fullstack software developer.</p>
+            <p>
+                I live in Plymouth, England with my wife and{' '}
+                <span
+                    className={classes.highlight}
+                    onMouseOver={() => console.log('ahoy')}
+                >
+                    two dogs
+                </span>
+                .
+            </p>
+        </div>
+    );
+
     return (
         <div className={classes.root}>
-            <div className={classes.profile}>
-                <div className={classes.imageContainer}>
-                    <Image
-                        src="https://res.cloudinary.com/stuj89/image/upload/v1725542567/Stu.jpg"
-                        alt="Stu"
-                        fill
-                        sizes="100%"
-                        priority
-                    ></Image>
-                </div>
-                <div className={classes.titleContainer}>
-                    <h1>Stuart Jauncey</h1>
-                    <h2>Fullstack Software Developer</h2>
-                </div>
-            </div>
-            <div className={classes.descriptionContainer}>
-                <p>Hi!</p>
-                <p>
-                    My name is Stuart and I am a fullstack software developer.
-                </p>
-                <p>I live in Plymouth, England with my wife and two dogs.</p>
-            </div>
+            {profile}
+            {description}
         </div>
     );
 }
