ae1ebfb79e852637af7fbed4d51bce5abd44556a
Stu <stuartjauncey@hotmail.co.uk>
2024-09-19 14:43:26 +0100

    Update message content

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -26,59 +26,76 @@ const messages: Message[] = [
         id: '1',
         name: 'Curious Person',
         content:
-            'Hi! I saw your profile and just wanted to know a little more about you.',
+            'Hi! I saw your profile and was wondering if I could ask you a few questions about what you do?',
         alignment: 'right',
     },
     {
         id: '2',
         name: 'Stuart',
         content:
-            'Hey! No problem, I am a fullstack software developer with 3 years experience.',
+            'Hey, thank you for reaching out! I am a fullstack software developer with three years experience of working at a fast paced logistics marketplace start-up.',
         alignment: 'left',
     },
     {
         id: '3',
         name: 'Curious Person',
-        content: 'Where are you based?',
+        content: "That's great! Where are you based?",
         alignment: 'right',
     },
     {
         id: '4',
         name: 'Stuart',
-        content: 'I am based in Plymouth, England.',
+        content:
+            'I live in the city of Plymouth, which is on the south coast of Devon, England.',
         alignment: 'left',
         imageURL: Images.Plymouth,
     },
     {
         id: '5',
         name: 'Curious Person',
-        content: 'What coding languages do you know?',
+        content:
+            'What programming languages and technologies have you worked with?',
         alignment: 'right',
     },
     {
         id: '6',
         name: 'Stuart',
         content:
-            'I know HTML, CSS, JavaScript, React.js, PHP, Laravel, TypeScript and Next.js.',
+            'I have experience working with HTML, CSS, JavaScript, React.js, PHP, Laravel, TypeScript, Next.js, MongoDB and SQL.',
         alignment: 'left',
     },
     {
         id: '7',
+        name: 'Stuart',
+        content:
+            'I am also familiar with Git practices and Agile methodologies.',
+        alignment: 'left',
+    },
+    {
+        id: '8',
         name: 'Curious Person',
         content: 'Do you have any pets?',
         alignment: 'right',
     },
     {
-        id: '8',
+        id: '9',
         name: 'Stuart',
         content: 'Yes! I have two dogs, Luna and Albert.',
         alignment: 'left',
         imageURL: Images.Dogs,
     },
     {
-        id: '9',
+        id: '10',
+        name: 'Curious Person',
+        content:
+            'Thank you for your time Stuart, it was a pleasure getting to know more about you!',
+        alignment: 'right',
+    },
+    {
+        id: '11',
         name: 'Stuart',
-        content: 'Thanks for taking out the time to learn more about me!',
+        content:
+            "You're very welcome! If you would like to know more please don' hesitate to get it touch.",
         alignment: 'left',
         imageURL: Images.Stu,
     },
